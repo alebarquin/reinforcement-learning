4e10323085e4490444667708c71d71286c5b4c09
Alejandro <alejandro.barquin@gmail.com>
2022-08-09 09:10:11 -0300

    finished rockets game

--- aigames/train.py
+++ aigames/train.py
@@ -6,6 +6,7 @@ from environment import Environment
 # Creating the bots
 class Route():
 
+     # Constructor method
      def __init__(self, dnaLength) -> None:
           self.dnaLength = dnaLength
           self.dna = list()
@@ -18,7 +19,6 @@ class Route():
                     rnd = np.random.randint(1, self.dnaLength)
                self.dna.append(rnd)
           self.dna.append(0)
-          # dna = 3,1,2,0
      
      # Building the Crossover method
      def mix(self, dna1, dna2):
@@ -52,3 +52,86 @@ class Route():
                          self.dna.pop(prevInx)
                     else:
                          self.dna.pop(prevInx + 1)
+
+# Initializing the main code
+populationSize = 50 # I could define anything here
+mutationRate = 0.1 
+nSelected = 5 # Number of bots selected for each iteration
+
+env = Environment()
+dnaLength = len(env.planets)
+population = list()
+
+# Creating the first population
+for i in range(populationSize):
+     route = Route(dnaLength)
+     population.append(route)
+
+# Starting the main loop
+generation = 0
+bestDist = np.inf # Initial value, will get better
+
+while True:
+     generation += 1
+
+     # Evaluating the population
+     for route in population:
+
+          env.reset()
+
+          for i in range(dnaLength):
+               action = route.dna[i]
+               route.distance += env.step(action, 'none')
+     
+     # Sorting the population
+     sortedPopulation = sorted(population, key = lambda x: x.distance) # Sort the bots from the current population so the best performing bots are on top
+     population.clear()
+
+     # Update the best distance found
+     if sortedPopulation[0].distance < bestDist:
+          bestDist = sortedPopulation[0].distance
+     
+     # Adding the best performing bots to the new population (optional in Genetic Algorithms)
+     for i in range(nSelected):
+          best = sortedPopulation[i]
+          best.distance = 0 # Reset the bot distance so it's not increased on top of that again (while taking a step)
+          population.append(best)
+
+     # Filling in the rest of the population
+     left = populationSize - nSelected
+     for i in range(left):
+          newRoute = Route(dnaLength)
+          # The new bot could be a complete new random, or an offspring of a previous bot!
+          if np.random.rand() <= mutationRate:
+               # If it's random, then just append to population list
+               population.append(newRoute)
+          else:
+               # If it's an offspring, then I need a previous bot and mutate it
+               inx1 = np.random.randint(0, nSelected)
+               inx2 = np.random.randint(0, nSelected)
+               while inx1 == inx2:
+                    inx2 = np.random.randint(0, nSelected)
+               
+               dna1 = sortedPopulation[inx1].dna
+               dna2 = sortedPopulation[inx2].dna
+
+               newRoute.mix(dna1, dna2)
+
+               population.append(newRoute)
+     
+     # Displaying the results
+     env.reset()
+
+     for i in range(dnaLength):
+          action = sortedPopulation[0].dna[i]
+          _ = env.step(action, 'normal')
+
+     if generation % 100 == 0:
+          env.reset()
+
+          for i in range(dnaLength):
+               action = sortedPopulation[0].dna[i]
+               _ = env.step(action, 'beautiful')
+     
+     print('Generation: ' + str(generation) + 'Shortest distance: {:.2f}'.format(bestDist) + 'light years')
+
