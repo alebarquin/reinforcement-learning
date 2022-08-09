2de7cf5498d57ae0c32d0cce1a808e092402d9ce
Alejandro <alejandro.barquin@gmail.com>
2022-08-09 08:15:46 -0300

    added aigames

---
+++ aigames/train.py
@@ -0,0 +1,54 @@
+# Space discover: Genetic Algorithms
+
+import numpy as np
+from environment import Environment
+
+# Creating the bots
+class Route():
+
+     def __init__(self, dnaLength) -> None:
+          self.dnaLength = dnaLength
+          self.dna = list()
+          self.distance = 0
+
+          # Initializing random DNA
+          for i in range(self.dnaLength - 1):
+               rnd = np.random.randint(1, self.dnaLength)
+               while rnd in self.dna:
+                    rnd = np.random.randint(1, self.dnaLength)
+               self.dna.append(rnd)
+          self.dna.append(0)
+          # dna = 3,1,2,0
+     
+     # Building the Crossover method
+     def mix(self, dna1, dna2):
+          
+          self.dna = dna1.copy()
+
+          for i in range(self.dnaLength - 1):
+               # 50% chances of having a mutation for each gene
+               if np.random.rand() <= 0.5:
+                    previous = self.dna[i]
+                    inx = self.dna.index(dna2[i])
+                    self.dna[inx] = previous
+                    self.dna[i] = dna2[i]
+          
+          # Random partial mutations 1
+          for i in range(self.dnaLength - 1):
+               # Only 10% chances of having a random mutation for each gene
+               if np.random.rand() <= 0.1:
+                    previous = self.dna[i]
+                    rnd = np.random.randint(1, self.dnaLength)
+                    inx = self.dna.index(rnd)
+                    self.dna[inx] = previous
+                    self.dna[i] = rnd
+
+               elif np.random.rand() <= 0.1:
+                    rnd = np.random.randint(1, self.dnaLength)
+                    prevInx = self.dna.index(rnd)
+                    self.dna.insert(i, rnd)
+
+                    if i >= prevInx:
+                         self.dna.pop(prevInx)
+                    else:
+                         self.dna.pop(prevInx + 1)
