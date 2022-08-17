41338c953cc5759b9d5d8240abe215709477a7c7
Alejandro <alejandro.barquin@gmail.com>
2022-08-17 07:04:27 -0300

    added many new codes

---
+++ custom-gym/envs/custom_env_dir/custom_env.py
@@ -0,0 +1,13 @@
+import gym
+
+class customEnv(gym.Env):
+
+    def __init__(self):
+        print('Environment initialized')
+        pass
+
+    def step(self):
+        pass
+
+    def reset(self):
+        pass
---
+++ stable-baselines3/SAC/snake-save.py
@@ -0,0 +1,25 @@
+import os
+import gym
+from stable_baselines3 import SAC
+from snakeEnv import SnakeEnv
+
+models_dir = 'models/SAC'
+logs_dir = 'logs/SAC'
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+env = SnakeEnv()
+
+model = SAC('MlpPolicy', env, verbose=1, tensorboard_log=logs_dir)
+
+timesteps = 10000
+for i in range(1, 10000):
+    
+    model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name='SAC')
+    model.save(f'{models_dir}/{timesteps*i}')
+
+env.close()
